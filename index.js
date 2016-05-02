4ac5db7020ec79b2232f48e3140c895af5aaf2ca
Osa Isacson <asaisacson@gmail.com>
2016-05-02 20:09:02 +0000

    exercise 3

--- index.js
+++ index.js
@@ -1,18 +1,86 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello', function (req, res) {
+
+function sayHelloTo(name) {
+  return "<h1>Hello _" + name + "_!</h1>";
+}
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function sub(num1, num2) {
+  return num1 - num2;
+}
+
+function mult(num1, num2) {
+  return num1 * num2;
+}
+
+function div(num1, num2) {
+  return num1 / num2;
+}
+
+
+
+
+app.get('/hello', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/hello/:name', function(req, res) {
+  var name = req.params.name;
+  var result = sayHelloTo(name);
+  res.send(result);
+});
 
+app.get('/calculator/:operation/:num1/:num2', function(req, res) {
+  var operation = req.params.operation; //identifies where we want to go in the object
+  var num1 = Number(req.params.num1); //converts our string to a number, says where it is and assigns it to the var num 1
+  var num2 = Number(req.params.num2);
+  var result;
+  if (operation === "add") {
+    var solution = add(num1, num2);
+    result = solution;
+  }
+  else if (operation === "sub") {
+    var solution = sub(num1, num2);
+    result = solution;
+  }
+  else if (operation === "mult") {
+    var solution = mult(num1, num2);
+    result = solution;
+  }
+  else if (operation === "div") {
+    var solution = div(num1, num2);
+    result = solution;
+  }
+  else {
+    result = "error"
+  }
+
+  var fullAnswer = {
+    "operator": operation,
+    "firstOperand": num1,
+    "secondOperand": num2,
+    "solution": result
+  };
+
+  if (result === "error") {
+    res.status(404).send("your shiat don't work")
+  }
+  else {
+    res.send(fullAnswer); //takes our answer and sends it to the page. 
+  }
+});
 
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
 // Boilerplate code to start up the web server
-var server = app.listen(process.env.PORT, process.env.IP, function () {
+var server = app.listen(process.env.PORT, process.env.IP, function() {
   var host = server.address().address;
   var port = server.address().port;
 
