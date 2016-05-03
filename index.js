8b958b7f1016301d89bb9d12c4175bbac75dd265
Osa Isacson <asaisacson@gmail.com>
2016-05-03 18:58:25 +0000

    fixes to the other questions, and answer to exercise 6

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
-var bodyParser = require('body-parser'); //reads a form's input and stores it as a javascript object accessible through `req.body` 
 var app = express();
+var bodyParser = require('body-parser'); //reads a form's input and stores it as a javascript object accessible through `req.body` 
+var cookieParser = require('cookie-parser');
+var mysql = require('mysql');
+var bcrypt = require('bcrypt');
+var redditAPI = require('./reddit.js')();
+app.use(cookieParser());
 app.use(bodyParser());
 
 
@@ -24,19 +29,40 @@ function div(num1, num2) {
   return num1 / num2;
 }
 
-function
-
-
-app.get('/hello', function(req, res) {
-  res.send('Hello World!');
+app.use(function(req, res, next) { //Middleware. This particular bit will run for every request done (because we are using 'use').
+  console.log(req.cookies);
+  next();
 });
 
+
+
+//Exercise 1: Create a web server that can listen to requests for /hello, and respond with some HTML that says <h1>Hello World!</h1>
+app.get('/hello', function(req, res) { //if you detect any incoming requests where the method is get, then run this function. request is like the input to your function. response is an object with what you send(specific to the stuff you're trying to do). send is like return. 
+  if (req.cookies.nameOfUser) {
+    res.send('Welcome back' + req.cookies.name.nameOfUser + '!');
+  }
+  else {
+    res.send('Hello World!');
+  }
+});
+
+
+//Exercise 2: Create a web server that can listen to requests for /hello?name=firstName, and respond with some HTML that says <h1>Hello _name_!</h1>. For example, if a client requests /hello/John, the server should respond with <h1>Hello John!</h1>
 app.get('/hello/:name', function(req, res) {
   var name = req.params.name;
   var result = sayHelloTo(name);
   res.send(result);
 });
 
+//alternatively...
+app.get('/hello2', function(req, res) {
+  var name = req.query.name;
+  res.cookie('nameOfUser', name); //stores the name var in a cookie, will send that cookie for every following request (nameOfUser=(the name you will enter in the response))
+  res.send('<h1>Hello' + name + '!</h1>');
+});
+
+
+//Exercise 3: Create a web server that can listen to requests for /calculator/:operation?num1=XX&num2=XX and respond with a JSON object
 app.get('/calculator/:operation/:num1/:num2', function(req, res) {
   var operation = req.params.operation; //identifies where we want to go in the object
   var num1 = Number(req.params.num1); //converts our string to a number, says where it is and assigns it to the var num 1
@@ -59,7 +85,7 @@ app.get('/calculator/:operation/:num1/:num2', function(req, res) {
     result = solution;
   }
   else {
-    result = "error"
+    result = "error";
   }
 
   var fullAnswer = {
@@ -70,13 +96,57 @@ app.get('/calculator/:operation/:num1/:num2', function(req, res) {
   };
 
   if (result === "error") {
-    res.status(404).send("your shiat don't work")
+    res.status(404).send("nat warking");
   }
   else {
     res.send(fullAnswer); //takes our answer and sends it to the page. 
   }
 });
 
+//...alternatively
+app.get('/calculator/:operation*?', function(request, response) { //the operation is part of the resource path, as the add, subtract, mult, div are dynamic and represented as functions.
+  if (!request.params.operation) {
+    response.status(400).send('you have to pass an operation');
+    return;
+  }
+  var operation = request.params.operation; //refers to the dynamic 'operation' part above (indicated by the : and the use of params), and says to store the particular request the user makes (add...) in the var 'operation'.
+  var num1 = request.query.num1; //
+  var num2 = request.query.num2;
+  if (operation === 'add') {
+    response.send({
+      op: 'add',
+      n1: num1,
+      n2: num2,
+      result: num1 + num2
+    });
+  }
+  else {
+    response.status(400).send({
+      error: 'bad operation'
+    });
+  }
+});
+
+
+//Exercise 4: Retrieving data from our database
+
+app.get('/posts', function(request, response) {
+  redditAPI.getAllPosts(function(err, posts) {
+    if (err) {
+      response.status(500).send('oops, try agan later')
+    }
+    else {
+      var listItems = posts.map(function(post) {
+        return '<li><a href="' + post.url + '">' + post.title + '</a>' + /*moment(post.createdAt).fromNow('') +*/ '</li>';
+        //can also use template stream literals: means you can put variables inside the stream as post.title etc
+      });
+      response.send('<ul>' + listItems.join(' ') + '</ul>');
+    }
+  })
+})
+
+
+//Exercise 5: In this exercise, we're going to use Express to simply send an HTML file to our user containing a <form>
 app.get('/createContent', function(req, res) {
 
   var options = {
@@ -98,17 +168,19 @@ app.get('/createContent', function(req, res) {
 });
 
 
-
-
+//Exercise 6: Receiving data from our form. In this exercise, we will write our first POST endpoint. The resource will be the same, /createContent, but we will be writing a second endpoint using app.post instead.
 
 app.post('/createContent', function(req, res) {
 
-  console.log(req.body)
+  console.log(req.body);
   var title = req.body.title;
   var URL = req.body.url;
-  
-  //call the function that insert the title and URL to mySQL
-  
+
+  redditAPI.createPost({userId: 1, url:URL, title: title},function(err, posts) {
+      res.send(posts);
+  });
+  //here call the function that insert the title and URL to mySQL
+
   res.send("OK");
 });
 
---
+++ reddit.js
@@ -0,0 +1,271 @@
+var bcrypt = require('bcrypt');
+var HASH_ROUNDS = 10;
+
+module.exports = function RedditAPI(conn) {
+  return {
+    createUser: function(user, callback) {
+
+      // first we have to hash the password...
+      bcrypt.hash(user.password, HASH_ROUNDS, function(err, hashedPassword) {
+        if (err) {
+          callback(err);
+        }
+        else {
+          conn.query(
+            'INSERT INTO `users` (`username`,`password`, `createdAt`) VALUES (?, ?, ?)', [user.username, hashedPassword, null],
+            function(err, result) {
+              if (err) {
+                /*
+                There can be many reasons why a MySQL query could fail. While many of
+                them are unknown, there's a particular error about unique usernames
+                which we can be more explicit about!
+                */
+                if (err.code === 'ER_DUP_ENTRY') {
+                  callback(new Error('A user with this username already exists'));
+                }
+                else {
+                  callback(err);
+                }
+              }
+              else {
+                /*
+                Here we are INSERTing data, so the only useful thing we get back
+                is the ID of the newly inserted row. Let's use it to find the user
+                and return it
+                */
+                conn.query(
+                  'SELECT `id`, `username`, `createdAt`, `updatedAt` FROM `users` WHERE `id` = ?', [result.insertId],
+                  function(err, result) {
+                    if (err) {
+                      callback(err);
+                    }
+                    else {
+                      /*
+                      Finally! Here's what we did so far:
+                      1. Hash the user's password
+                      2. Insert the user in the DB
+                      3a. If the insert fails, report the error to the caller
+                      3b. If the insert succeeds, re-fetch the user from the DB
+                      4. If the re-fetch succeeds, return the object to the caller
+                      */
+                      callback(null, result[0]);
+                    }
+                  }
+                );
+              }
+            }
+          );
+        }
+      });
+    },
+    createPost: function(post, callback) {
+      conn.query(
+        'INSERT INTO `posts` (`userId`, `title`, `url`, `createdAt`) VALUES (?, ?, ?, ?)', [post.userId, post.title, post.url, null],
+        function(err, result) {
+          if (err) {
+            callback(err);
+          }
+          else {
+            /*
+            Post inserted successfully. Let's use the result.insertId to retrieve
+            the post and send it to the caller!
+            */
+            conn.query(
+              'SELECT `id`,`title`,`url`,`userId`, `createdAt`, `updatedAt` FROM `posts` WHERE `id` = ?', [result.insertId],
+              function(err, result) {
+                if (err) {
+                  callback(err);
+                }
+                else {
+                  callback(null, result[0]);
+                }
+              }
+            );
+          }
+        }
+      );
+    },
+    getAllPosts: function(options, callback) {
+      // In case we are called without an options parameter, shift all the parameters manually
+      if (!callback) {
+        callback = options;
+        options = {};
+      }
+      var limit = options.numPerPage || 25; // if options.numPerPage is "falsy" then use 25
+      var offset = (options.page || 0) * limit;
+
+      conn.query(`
+        SELECT 
+        posts.id AS id, 
+        posts.title AS title, 
+        posts.url AS url, 
+        posts.userId AS postUserId, 
+        posts.createdAt AS postCreatedAt, 
+        posts.updatedAt AS postUpdatedAt, 
+        users.id AS userUserId, 
+        users.username AS userName, 
+        users.createdAt AS userCreatedAt, 
+        users.updatedAt AS userUpdatedAt
+        FROM posts
+        JOIN users
+        ON users.id=posts.userId
+        ORDER BY posts.createdAt DESC
+        LIMIT ? OFFSET ?
+        `, [limit, offset],
+        function(err, results) {
+          if (err) {
+            callback(err);
+          }
+          else {
+          var finalresults = results.map(function(current){
+                var joinedresults = {
+              "id": current.id,
+              "title": current.title,
+              "url": current.url,
+              "createdAt": current.postCreatedAt,
+              "updatedAt": current.postUpdatedAt,
+              "userId": current.postUserId,
+              "user": {
+                "id": current.userUserId,
+                "username": current.userName,
+                "userCreatedAt": current.userCreatedAt,
+                "userUpdatedAt": current.userUpdatedAt
+              }
+            };
+            return joinedresults;
+           });
+            callback(null, finalresults);
+
+          }
+        }
+
+      );
+    },
+    getAllPostsForUser: function(userId, options, callback) {
+      // In case we are called without an options parameter, shift all the parameters manually
+      if (!callback) {
+        callback = options;
+        options = {};
+      }
+      var limit = options.numPerPage || 25; // if options.numPerPage is "falsy" then use 25
+      var offset = (options.page || 0) * limit;
+
+      conn.query(`
+        SELECT 
+        posts.id AS id, 
+        posts.title AS title, 
+        posts.url AS url, 
+        posts.userId AS postUserId, 
+        posts.createdAt AS postCreatedAt, 
+        posts.updatedAt AS postUpdatedAt, 
+        users.id AS userUserId, 
+        users.username AS userName, 
+        users.createdAt AS userCreatedAt, 
+        users.updatedAt AS userUpdatedAt
+        FROM posts 
+        JOIN users 
+        ON users.id=posts.userId
+        WHERE posts.userId= ?
+        LIMIT ? OFFSET ? 
+        `, [userId, limit, offset],
+        function(err, results) {
+          if (err) {
+            callback(err);
+          }
+          else {
+          var finalResultUser = results.map(function(current){
+                var joinedresults = {
+              "id": current.id,
+              "title": current.title,
+              "url": current.url,
+              "postCreatedAt": current.postCreatedAt,
+              "postUpdatedAt": current.postUpdatedAt,
+              "userId": current.postUserId,
+              "user": {
+                "id": current.userUserId,
+                "username": current.userName,
+                "userCreatedAt": current.userCreatedAt,
+                "userUpdatedAt": current.userUpdatedAt
+              }
+            };
+            return joinedresults;
+           });
+           }
+            callback(null, finalResultUser);
+          }
+      );
+    },
+    getSinglePost: function(postId, callback) {
+ 
+
+      conn.query(`
+        SELECT 
+        posts.id AS id, 
+        posts.title AS title, 
+        posts.url AS url, 
+        posts.userId AS postUserId, 
+        posts.createdAt AS postCreatedAt, 
+        posts.updatedAt AS postUpdatedAt, 
+        users.id AS userUserId, 
+        users.username AS userName, 
+        users.createdAt AS userCreatedAt, 
+        users.updatedAt AS userUpdatedAt
+        FROM posts 
+        JOIN users 
+        ON users.id=posts.userId
+        WHERE posts.id= ?
+        `, [postId],
+        function(err, results) {
+          if (err) {
+            callback(err);
+          }
+          else {
+          var finalResultPost =  {
+              "id": results[0].id,
+              "title": results[0].title,
+              "url": results[0].url,
+              "postCreatedAt": results[0].postCreatedAt,
+              "postUpdatedAt": results[0].postUpdatedAt,
+              "userId": results[0].postUserId,
+              "user": {
+                "id": results[0].userUserId,
+                "username": results[0].userName,
+                "userCreatedAt": results[0].userCreatedAt,
+                "userUpdatedAt": results[0].userUpdatedAt
+              }
+            };
+           }
+            callback(null, finalResultPost);
+          }
+      );
+    },
+    createSubreddit: function(subreddits, callback){
+      conn.query(
+        'INSERT INTO `subreddits` (`name`, `description`) VALUES (?, ?)', [subreddits.name, subreddits.description],
+        function(err, result) {
+          if (err) {
+            callback(err);
+          }
+          else {
+            /*
+            Subreddit inserted successfully. Let's use the result.insertId to retrieve
+            the subreddit and send it to the caller!
+            */
+            conn.query(
+              'SELECT `name`,`description` FROM `subreddits` WHERE `id` = ?', [result.insertId],
+              function(err, result) {
+                if (err) {
+                  callback(err);
+                }
+                else {
+                  callback(null, result[0]);
+                }
+              }
+            );
+          }
+        }
+      );
+    }
+    };
+  };
+    
